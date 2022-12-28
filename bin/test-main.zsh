e0de45ece1635db632f50632c4e53a19f71f4d9e
Adam Kiss <iam@adamkiss.com>
2022-12-28 21:45:38 +0100

    Finish the select script

--- bin/test-main.zsh
+++ bin/test-main.zsh
@@ -16,11 +16,38 @@ fi
 
 /usr/bin/sqlite3 unicode.sqlite3 <<EOF
 ATTACH "./user.sqlite3" as user;
-WITH found (
-	SELECT *, rank, usage.count
-	FROM main.characters('$query')
+-- the query
+WITH found AS (
+	SELECT character, name, category, hex, html, json, user.usage.count
+	FROM main.characters('2192')
 	LEFT JOIN user.usage ON main.characters.hex = user.usage.id
-	ORDER BY user.usage.count, rank, length(name)
-	LIMIT 50;
+	ORDER BY user.usage.count DESC, rank ASC, length(name) ASC
+	LIMIT 50
 )
+-- the formatter
+select JSON_OBJECT(
+	'items', JSON_GROUP_ARRAY(
+		JSON_OBJECT(
+			'uid', hex,
+			'title', character,
+			'subtitle', name || " (" || category || ")",
+			'icon', JSON_OBJECT('path', 'icon.png'),
+			'arg', character,
+			'mods', JSON_OBJECT(
+				'cmd', JSON_OBJECT(
+					'arg', html,
+					'subtitle', 'Copy & paste as HTML'
+				),
+				'option', JSON_OBJECT(
+					'arg', json,
+					'subtitle', 'Copy & paste as JS/Python/…'
+				),
+				'ctrl', JSON_OBJECT(
+					'arg', hex,
+					'subtitle', 'Copy & paste as Decimal value'
+				)
+			)
+		)
+	)
+) FROM found;
 EOF
