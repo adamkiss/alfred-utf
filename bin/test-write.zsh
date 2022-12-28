a4d4dc8931ea40de822c369b876f3890a36933e2
Adam Kiss <iam@adamkiss.com>
2022-12-28 21:59:49 +0100

    fix file check in the write test script

--- bin/test-write.zsh
+++ bin/test-write.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 # Create the usage table if it doesn't exist
-if ! [[ -f usage.sqlite3 ]]; then
+if ! [[ -f user.sqlite3 ]]; then
     echo 'CREATE TABLE "usage" ("id" string UNIQUE NOT NULL,"count" integer NOT NULL DEFAULT 1, PRIMARY KEY (id));' | sqlite3 user.sqlite3
 fi
 
