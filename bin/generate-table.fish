e0f117d007b7923a24055f692be03f9e8017bb47
Adam Kiss <iam@adamkiss.com>
2022-12-28 21:37:14 +0100

    Hopefully final unicode table

--- bin/generate-table.fish
+++ bin/generate-table.fish
@@ -2,42 +2,37 @@
 
 # wrap argument "cat" as "category:$cat" and return formatted characters from the category
 function uni_category -a cat
-    ./uni print category:$cat -c -f "('%(char)', LOWER('%(name)'), '%(cat)', '%(hex)', '%(keysym)'),"
+    set capture (string join ',' (./uni print category:$cat -c -f "(%(hex q), %(char q), LOWER(%(name q)), %(cat q), %(html q), %(json q), %(keysym q))"))
+    string replace "'''" "''''" $capture # fucking apostrophe
 end
 
-# join the results of (uni_category $cat) and trim trailing ','
-function uni_to_values -a cat
-    set return (string trim -r -c , (string join '' (uni_category $cat)))
-    # escape APOSTROPHE for the SQL
-    string replace "'''" '"\'"' $return
-end
-
-function insert_into_characters -a values
-    echo "INSERT INTO characters (character, name, category, hex, keysymbol) VALUES $values;"
+function insert_into_characters -a cat
+    echo "INSERT INTO characters (hex, character, name, category, html, json, keysymbol) VALUES $(uni_category $cat);"
 end
 
 # Create the unicode characters database
 echo "
 DROP TABLE IF EXISTS characters;
 CREATE VIRTUAL TABLE characters USING fts5 (
+    hex,
     character UNINDEXED,
     name,
     category,
-    hex,
-    html,
+    html UNINDEXED,
+    json,
     keysymbol,
     tokenize = 'porter unicode61'
 );
-$(insert_into_characters (uni_to_values C))
-$(insert_into_characters (uni_to_values L))
-$(insert_into_characters (uni_to_values M))
-$(insert_into_characters (uni_to_values P))
-$(insert_into_characters (uni_to_values S))
-$(insert_into_characters (uni_to_values Z))
+$(insert_into_characters C)
+$(insert_into_characters L)
+$(insert_into_characters M)
+$(insert_into_characters P)
+$(insert_into_characters S)
+$(insert_into_characters Z)
 " > generate-table.sql # it's ran from main folder
 
 # run the SQL and generate the table
 /usr/bin/sqlite3 unicode.sqlite3 < generate-table.sql
 
 # remove the file
-rm generate-table.sql
+# rm generate-table.sql
