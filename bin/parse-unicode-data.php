56a235d14a8034ac36f1fdd84b2258ec31034580
Adam Kiss <iam@adamkiss.com>
2022-12-29 22:51:35 +0100

    parse Unicode data and come up with meaningful alt names

--- bin/parse-unicode-data.php
+++ bin/parse-unicode-data.php
@@ -2,6 +2,8 @@
 
 # utils
 function a(...$arg) { return $arg; }
+# make intellephense shut up
+if (!function_exists('ray')) { function ray(...$arg) { return $arg; }}
 
 # Latest unicode data: https://www.unicode.org/Public/UCD/latest/ucd/
 
@@ -31,13 +33,43 @@ while(! $datafile->eof()) {
 
     $character = a(
         hex: $raw[0] === '0000' ? '0' : ltrim(strtolower($raw[0]), '0'),
-        name: mb_convert_case($raw[1], MB_CASE_TITLE),
-        alt: mb_convert_case($raw[10], MB_CASE_TITLE)
+        name: mb_strtolower($raw[1]),
+        alt: mb_strtolower($raw[10])
     );
 
-    // Get only the difference between the alt and name, and save that
+    // Simplify alt names
     $diffAlt  = explode(' ', $character['alt']);
     $diffName = explode(' ', $character['name']);
+    $diffNamePreg = preg_split('/[ -]/', $character['name']);
+
+    // if name fully contains alt, remove alt
+    if (empty(array_diff($diffAlt, $diffName))) {
+        $character['alt'] = '';
+        continue;
+    }
+
+    // if alt is only two words instead of name's two-words, ignore
+    if (empty(array_diff($diffAlt, $diffNamePreg))) {
+        $character['alt'] = '';
+        continue;
+    }
+
+    // if alt contains more info (like Georgian >small< letter Han),
+    // replace name with alt
+    if (empty(array_diff($diffName, $diffAlt))) {
+        $character['name'] = $character['alt'];
+        $character['alt'] = '';
+        continue;
+    }
+
+    // replace <control> with alt name and ignore
+    if ($character['name'] === '<control>') {
+        $character['name'] = $character['alt'];
+        $character['alt'] = '';
+        continue;
+    }
+
+    // Get only the difference between the alt and name, and save that
     while ((!empty($diffAlt) && !empty($diffName)) && $diffAlt[0] === $diffName[0]) {
         array_shift($diffAlt);
         array_shift($diffName);
@@ -47,5 +79,21 @@ while(! $datafile->eof()) {
     $data []= $character;
 }
 
-// Too many
+// Now get only character that have alt name
 $altNameCharacters = array_filter($data, fn($ch) => !empty($ch['alt']));
+
+
+// OBSERVE
+if ($argv[1] ?? false === 'debug') {
+    // ignore-next-line
+    ray(count($altNameCharacters));
+    file_put_contents(
+        __DIR__ . '/diff-name-alt.txt', 
+        implode("\n", array_map(fn($ch) => "{$ch['hex']} => {$ch['name']} / {$ch['alt']}", $altNameCharacters))
+    );
+} else {
+    echo implode('', array_map(
+        fn($ch) => "UPDATE characters SET a = '{$ch['alt']}' WHERE hex = '{$ch['hex']}';",
+        $altNameCharacters
+    ));
+}
